f87000a5e78acc17a09a7a3f28a28882e3036b1f
Rakshith N <rakshithacchu9625@gmail.com>
2026-07-19 12:06:03 +0530

    feat: add MetaOptionsModule and update TypeOrm configuration to use environment variables

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-//custom modules
+//TypeOrm Module
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+//Custom Modules
 import { UsersModule } from 'src/modules/users/users.module';
 import { PostsModule } from '../posts/posts.module';
 import { AuthModule } from '../auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagsModule } from '../tags/tags.module';
+import { MetaOptionsModule } from '../meta-options/meta-options.module';
 // entities will be auto-scanned by glob pattern below
 
 
@@ -15,6 +21,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     UsersModule,
     PostsModule,
     AuthModule,
+    TagsModule,
+    MetaOptionsModule,
     TypeOrmModule.forRootAsync({
       imports: [],
       inject: [],
@@ -24,11 +32,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
         poolSize: 20,
-        port: 5432,
-        username: "postgres",
-        password: "postgres",
-        host: "localhost",
-        database: "nestjs-blog",
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        // SSL config: try to load a CA bundle from ./certs/ca.pem (project root: src/modules/app/../../..)
+        // If no CA is present, fall back to allowing self-signed certs (rejectUnauthorized: false).
+        ssl: (() => {
+          try {
+            const caPath = path.join(__dirname, '..', '..', '..', 'certificates', 'db-ca.pem');
+            if (fs.existsSync(caPath)) {
+              return { ca: fs.readFileSync(caPath).toString() };
+            }
+          } catch (e) {
+            // ignore and fall back
+          }
+          return { rejectUnauthorized: true } as any;
+        })()
         // schema: "custom"
       }))
     })
